dd7804a0328ed3e8b69c8dbd7235b91c5f2c6ee9
Alexandr Kruhlov <alkruglov777@gmail.com>
2018-04-12 17:02:16 +0300

    Deleted resource, change the container method from ClassPathXmlApplicationContext to AnnotationConfigApplicationContext

--- src/main/java/ua/rafael/App.java
+++ src/main/java/ua/rafael/App.java
@@ -2,14 +2,11 @@ package ua.rafael;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-				
-//				ClassPathXmlApplicationContext("spring-conf.xml");
 		MessageRenderer messageRenderer = context.getBean("messageRenderer", MessageRenderer.class);
 		messageRenderer.render();
 	}
