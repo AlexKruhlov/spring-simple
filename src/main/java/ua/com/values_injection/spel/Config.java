3cdd379bce95cedc606fc25b0c117f069e092c2e
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-04-17 00:01:22 +0300

    Added spel values injection type

---
+++ src/main/java/ua/com/values_injection/spel/Config.java
@@ -0,0 +1,9 @@
+package ua.com.values_injection.spel;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ua.com.values_injection.spel")
+public class Config {
+}
---
+++ src/main/java/ua/com/values_injection/spel/InjectConfig.java
@@ -0,0 +1,52 @@
+package ua.com.values_injection.spel;
+
+import org.springframework.stereotype.Component;
+
+@Component("injectConfig")
+public class InjectConfig {
+	private String name = "Chris Baker";
+	private int age = 32;
+	private float heigh = 1.788f;
+	private boolean programer = true;
+	private long ageInSeconds = 1009843200L;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getHeigh() {
+		return heigh;
+	}
+
+	public void setHeigh(float heigh) {
+		this.heigh = heigh;
+	}
+
+	public boolean isProgramer() {
+		return programer;
+	}
+
+	public void setProgramer(boolean programer) {
+		this.programer = programer;
+	}
+
+	public long getAgeInSeconds() {
+		return ageInSeconds;
+	}
+
+	public void setAgeInSeconds(long ageInSeconds) {
+		this.ageInSeconds = ageInSeconds;
+	}
+}
