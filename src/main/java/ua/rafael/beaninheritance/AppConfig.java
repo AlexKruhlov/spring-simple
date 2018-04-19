1cfbc390c9735f7ce01dd90a5915d630c8c27b3b
Alexandr Kruhlov <alkruglov777@gmail.com>
2018-04-19 16:05:01 +0300

    Added bean inheritance

---
+++ src/main/java/ua/rafael/beaninheritance/AppConfig.java
@@ -0,0 +1,34 @@
+package ua.rafael.beaninheritance;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public Mouse mouse() {
+		Mouse mouse = new Mouse();
+		initAnimal(mouse);
+		mouse.setAge(25);
+		mouse.setLocation("Europe");
+		return mouse;
+	}
+
+	private void initAnimal(Animal animal) {
+		animal.setAge(3);
+		animal.setName("Animal");
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext();
+		((AnnotationConfigApplicationContext) context).register(AppConfig.class);
+		((AnnotationConfigApplicationContext) context).refresh();
+		Mouse mouse = context.getBean(Mouse.class);
+		System.out.println("Age " + mouse.getAge());
+		System.out.println("Name " + mouse.getName());
+		System.out.println("Location " + mouse.getLocation());
+	}
+}
