c6760939f242675e3b6f1b91de157dc07442b7dc
Alexandr Kruhlov <alkruglov777@gmail.com>
2018-04-20 15:26:09 +0300

    Added bean initialization feature

---
+++ src/main/java/ua/rafael/beaninit/Config.java
@@ -0,0 +1,9 @@
+package ua.rafael.beaninit;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ua.rafael.beaninit")
+public class Config {
+}
