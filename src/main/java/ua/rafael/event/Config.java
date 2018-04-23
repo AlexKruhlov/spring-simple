1d0691e820c0143b766f83eb3841bc5198eaa21c
Alexandr Kruhlov <alkruglov777@gmail.com>
2018-04-23 17:32:21 +0300

    Added event listening feature

---
+++ src/main/java/ua/rafael/event/Config.java
@@ -0,0 +1,9 @@
+package ua.rafael.event;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ua.rafael.event")
+public class Config {
+}
